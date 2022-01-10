d8628e041542d475c5327368c7ca3809ac5250a3
Vadzim Dzianisik <wadik.nhl@gmail.com>
2022-01-10 13:26:31 +0300

    feat: add provider and middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 import { App } from 'App';
+import { store } from 'store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 reportWebVitals();
 export {};
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
-import { combineReducers, createStore, Store } from 'redux';
+import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import { auth, login } from 'store/reducers';
 
@@ -9,7 +10,10 @@ const reducers = combineReducers({
 
 export type AppStoreType = ReturnType<typeof reducers>;
 
-export const store: Store<AppStoreType> = createStore(reducers);
+export const store: Store<AppStoreType> = createStore(
+  reducers,
+  applyMiddleware(thunkMiddleware),
+);
 
 // @ts-ignore
 window.store = store; // for dev
